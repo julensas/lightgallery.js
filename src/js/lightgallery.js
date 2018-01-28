2c1d5f604378271c8dea0e4fbe702857e93885f7
julensas <julius.baltrusaitis@gmail.com>
2018-01-28 13:53:44 +0200

    Update lightgallery.js

--- src/js/lightgallery.js
+++ src/js/lightgallery.js
@@ -1335,8 +1335,8 @@ Plugin.prototype.destroy = function(d) {
         utils.trigger(_this.el, 'onBeforeClose');
     }
 
-    document.body.scrollTop = _this.prevScrollTop;
-    document.documentElement.scrollTop = _this.prevScrollTop;
+    // document.body.scrollTop = _this.prevScrollTop;
+    // document.documentElement.scrollTop = _this.prevScrollTop;
 
     /**
      * if d is false or undefined destroy will only close the gallery
